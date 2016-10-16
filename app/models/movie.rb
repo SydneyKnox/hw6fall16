0e387350664883d3bfc5d34320fd2d6540fb1d1f
Sydney Knox <sydney-knox@uiowa.edu>
2016-10-16 18:34:13 +0000

    switching over after cloning without forking... search tmdb view created, search tmdb function implemented, find tmdb function implemented, add tmdb function still to be implemented

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,14 +3,30 @@ class Movie < ActiveRecord::Base
     %w(G PG PG-13 NC-17 R)
   end
   
-# class Movie::InvalidKeyError < StandardError ; end
+ class Movie::InvalidKeyError < StandardError ; end
   
-#  def self.find_in_tmdb(string)
-#    begin
-#      Tmdb::Movie.find(string)
-#    rescue Tmdb::InvalidApiKeyError
-#        raise Movie::InvalidKeyError, 'Invalid API key'
-#    end
-#  end
+  def self.find_in_tmdb(string)
+    begin
+      Tmdb::Movie.find(string)
+    rescue Tmdb::InvalidApiKeyError
+        raise Movie::InvalidKeyError, 'Invalid API key'
+    end
+  end
+  
+  def self.create_from_tmdb(id)
+      Tmdb::Api.key("f4702b08c0ac6ea5b51425788bb26562")
+      movie = Tmdb::Movie.detail(id)
+      usa = Tmdb::Movie.releases(movie.id)["countries"].find {|rating| rating['iso_3166_1'] == "US"}
+        if(usa != nil)
+          rating = usa["certification"]
+        else
+          rating = "NR"
+        end #title rating description release_date
+      puts id
+      puts movie.title
+      puts movie.overview
+      Movie.create!({:movie => id, :title => movie.title, :rating => rating, :description => movie.overview, :release_date => movie.release_date})
+      flash[:warning] = "movies created"
+  end
 
 end
